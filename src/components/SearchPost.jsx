10a658fbc65d191b9bda229301d53b2f72d49fcf
Snehatk2002 <sammu5285@gmail.com>
2024-06-07 21:08:16 +0530

    completed

---
+++ src/components/SearchPost.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const SearchPost = () => {
+  return (
+    <div>
+        <Navbar/>
+        <div className="container">
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <div className="row g-3">
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <label htmlFor="" className="form-label">USER NAME</label>
+                            <input type="text" className="form-control" /> 
+                        </div>
+                        <center>
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <button class="btn btn-success">SEARCH</button>
+                        </div>
+                        </center>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default SearchPost
